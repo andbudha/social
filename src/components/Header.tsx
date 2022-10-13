9d7781adc36812fa367cc1d84c39b868672f9009
andbudha <andreibartov@gmail.com>
2022-10-13 21:37:25 +0200

    components created and updated

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const Header =()=> {
+    return <header className='header'>
+                <img src="https://images-platform.99static.com//X2Itn0ho4uuz80gMnEqcBmdbJnY=/1230x635:1730x1135/fit-in/590x590/99designs-contests-attachments/75/75719/attachment_75719077" alt=""/>
+            </header>
+}
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+const Profile =()=> {
+    return  <div className='content'>
+        <div>
+            <img src="https://c4.wallpaperflare.com/wallpaper/244/947/795/cool-berlin-wallpaper-83635-wallpaper-preview.jpg" alt=""/>
+        </div>
+        <div>
+            avatar + description
+        </div>
+        <div>
+            My Posts
+            <div>
+                New Posts
+            </div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
