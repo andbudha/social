45d22af9b8b310285ec9939b58d81335df1082c4
andbudha <andreibartov@gmail.com>
2023-09-24 21:32:32 +0200

    state file/react and routerdom updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,32 @@
 import React from 'react';
 import './App.css';
-import Header from "./components/Header/Header";
-import Navbar from "./components/Navbar/Navbar";
-import Profile from "./components/Profile/Profile";
-import Dialogues from "./components/Dialogues/Dialogues";
-import {Route} from "react-router-dom";
-import {RootStateType} from "./redux/state";
-
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile';
+import Dialogues from './components/Dialogues/Dialogues';
+import { Route } from 'react-router-dom';
+import { RootStateType } from './redux/state';
 
 type AppPropsType = {
-    state: RootStateType
-    addPost: (postMessage: string)=> void
-}
+  state: RootStateType;
+  addPost: (postMessage: string) => void;
+};
 
-const App =(props: AppPropsType)=> {
+const App = (props: AppPropsType) => {
+  return (
+    <div className="app-wrapper">
+      <Header />
+      <Navbar />
+      <div className="app-wrapper-content">
+        <Route
+          path="/profile/"
+          element={<Profile state={props.state} addPost={props.addPost} />}
+        />
 
-    return(
-            <div className='app-wrapper'>
-                <Header />
-                <Navbar />
-                <div className='app-wrapper-content'>
-                    <Route
-                        path='/profile/'
-                        render={()=><Profile
-                            state={props.state}
-                            addPost={props.addPost}
-                        />}/>
-
-                    <Route
-                        path='/dialogues/'
-                        render={()=><Dialogues
-                            state={props.state}
-                        />}/>
-                </div>
-            </div>
-    );
-}
+        <Route path="/dialogues/" element={<Dialogues state={props.state} />} />
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -1,69 +1,67 @@
-import {rerenderEntireTree} from "../render";
+import { rerenderEntireTree } from '../render';
 
 export type MessageType = {
-    id: number
-    message: string
-}
+  id: number;
+  message: string;
+};
 
 export type DialogueType = {
-    id: number
-    name: string
-}
+  id: number;
+  name: string;
+};
 
 export type PostType = {
-    id: number
-    message: string
-    likeCount: number
-}
+  id: number;
+  message: string;
+  likeCount: number;
+};
 
 type MessagePageType = {
-    dialogues: Array<DialogueType>
-    messages: Array<MessageType>
-}
+  dialogues: Array<DialogueType>;
+  messages: Array<MessageType>;
+};
 
 type ProfilePageType = {
-    posts: Array<PostType>
-}
+  posts: Array<PostType>;
+};
 
- export type RootStateType = {
-    profilePage: ProfilePageType
-    messagePage: MessagePageType
-}
+export type RootStateType = {
+  profilePage: ProfilePageType;
+  messagePage: MessagePageType;
+};
 
-
-let state = {
-    profilePage: {
-        posts: [
-            {id: 1, message: 'Hi...', likeCount: 25},
-            {id: 2, message: 'How are you?', likeCount: 52}
-        ]
-    }
-    ,
-    messagePage: {
-        dialogues: [
-            {id: 1, name: 'Andrei'},
-            {id: 2, name: 'Delia'},
-            {id: 3, name: 'Natalia'},
-            {id: 4, name: 'Dorian'}
-        ],
-        messages: [
-            {id: 1, message: 'Hi...'},
-            {id: 2, message: 'How are you?'},
-            {id: 3, message: 'Are you free today?'}
-        ]
-    }
-}
+let state: RootStateType = {
+  profilePage: {
+    posts: [
+      { id: 1, message: 'Hi...', likeCount: 25 },
+      { id: 2, message: 'How are you?', likeCount: 52 },
+    ],
+  },
+  messagePage: {
+    dialogues: [
+      { id: 1, name: 'Andrei' },
+      { id: 2, name: 'Delia' },
+      { id: 3, name: 'Natalia' },
+      { id: 4, name: 'Dorian' },
+    ],
+    messages: [
+      { id: 1, message: 'Hi...' },
+      { id: 2, message: 'How are you?' },
+      { id: 3, message: 'Are you free today?' },
+    ],
+  },
+};
 
 export const addPost = (postMessage: string) => {
-    debugger
-    const newPost: PostType = {
-        id: 5,
-        message: postMessage,
-        likeCount: 13
-    };
+  debugger;
+  const newPost: PostType = {
+    id: 5,
+    message: postMessage,
+    likeCount: 13,
+  };
 
   state.profilePage.posts.push(newPost);
   rerenderEntireTree(state);
-}
+};
 
 export default state;
