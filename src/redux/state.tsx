5df91206881664e094eb966f7735c3c3d96aff6f
andbudha <andreibartov@gmail.com>
2023-09-25 15:27:07 +0200

    custom store object created

--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -1,11 +1,3 @@
-let onPostAdding = () => {
-  console.log('Hello');
-};
-
-export const subscribe = (observer: () => void) => {
-  onPostAdding = observer;
-};
-
 export type PostType = {
   id: number;
   message: string | undefined;
@@ -36,6 +28,66 @@ export type StateType = {
   messagePage: MessagePageType;
 };
 
+export type StoreType = {
+  _state: StateType;
+  _onPostAdding: () => void;
+  addPost: () => void;
+  updatePostText: (newText: string) => void;
+  subscribe: (observer: () => void) => void;
+  getState: () => StateType;
+};
+
+//store
+export const store: StoreType = {
+  _state: {
+    profilePage: {
+      posts: [
+        { id: 1, message: 'Hi, how are you?', likeCount: 15 },
+        { id: 2, message: 'It is my first post!', likeCount: 25 },
+      ],
+      newPostText: '',
+    },
+    messagePage: {
+      participants: [
+        { id: 1, name: 'Dorian' },
+        { id: 2, name: 'Delia' },
+        { id: 3, name: 'Natalia' },
+        { id: 4, name: 'Andrei' },
+      ],
+      messages: [
+        { id: 1, message: 'Hey, there...are you ok?' },
+        { id: 2, message: 'Still mad with me?' },
+        { id: 3, message: "I will be at Joe's." },
+        {
+          id: 4,
+          message: 'Never mind, I am still interested in this position.',
+        },
+      ],
+    },
+  },
+  getState() {
+    return this._state;
+  },
+  _onPostAdding() {},
+  addPost() {
+    this._state.profilePage.posts.push({
+      id: 999,
+      message: this._state.profilePage.newPostText,
+      likeCount: 6,
+    });
+    this._state.profilePage.newPostText = '';
+    this._onPostAdding();
+  },
+  updatePostText(newText: string) {
+    this._state.profilePage.newPostText = newText;
+    this._onPostAdding();
+  },
+  subscribe(observer: () => void) {
+    this._onPostAdding = observer;
+  },
+};
+
+//state
 export const state: StateType = {
   profilePage: {
     posts: [
@@ -58,6 +110,12 @@ export const state: StateType = {
       { id: 4, message: 'Never mind, I am still interested in this position.' },
     ],
   },
+};
+
+let onPostAdding = () => {};
+
+export const subscribe = (observer: () => void) => {
+  onPostAdding = observer;
 };
 
 //post adding func
