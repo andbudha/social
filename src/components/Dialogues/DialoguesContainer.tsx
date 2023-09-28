4d7fb8a81a505cae8a39f531ab293ae1e819fe73
andbudha <andreibartov@gmail.com>
2023-09-28 12:08:06 +0200

    connect method in DialoguesContainer applied

--- src/components/Dialogues/DialoguesContainer.tsx
+++ src/components/Dialogues/DialoguesContainer.tsx
@@ -1,35 +1,66 @@
-import { MessagePageType } from '../../types/store-types';
+import { StateType } from '../../types/store-types';
 import { Dialogues } from './Dialogues';
 import {
   addMessageAC,
   updateMessageTextAC,
 } from '../../redux/dialogue-reducer';
-import { ActionTypes } from '../../types/action-types';
+import { connect } from 'react-redux';
 
-type DialoguesConainerPropsType = {
-  messagePage: MessagePageType;
-  newMessageText: string;
-  dispatch: (action: ActionTypes) => void;
-};
-export const DialoguesContainer: React.FC<DialoguesConainerPropsType> = (
-  props
-) => {
-  //input value catching handler
-  const updateMessageValue = (newMessageValue: string) => {
-    props.dispatch(updateMessageTextAC(newMessageValue));
+const mapStateToProps = (state: StateType) => {
+  return {
+    newMessageText: state.messagePage.newMessageText,
+    messagePage: state.messagePage,
   };
-  //message adding func
-  const addMessage = () => {
-    props.dispatch(addMessageAC());
-  };
-  return (
-    <div>
-      <Dialogues
-        messagePage={props.messagePage}
-        newMessageText={props.newMessageText}
-        updateMessageValue={updateMessageValue}
-        addMessage={addMessage}
-      />
-    </div>
-  );
 };
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    updateMessageValue: (newMessageValue: string) => {
+      dispatch(updateMessageTextAC(newMessageValue));
+    },
+    addMessage: () => {
+      dispatch(addMessageAC());
+    },
+  };
+};
+export const connectDialogueContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Dialogues);
+
+//OLD CODE
+
+// import { MessagePageType } from '../../types/store-types';
+// import { Dialogues } from './Dialogues';
+// import {
+//   addMessageAC,
+//   updateMessageTextAC,
+// } from '../../redux/dialogue-reducer';
+// import { ActionTypes } from '../../types/action-types';
+
+// type DialoguesConainerPropsType = {
+//   messagePage: MessagePageType;
+//   newMessageText: string;
+//   dispatch: (action: ActionTypes) => void;
+// };
+// export const DialoguesContainer: React.FC<DialoguesConainerPropsType> = (
+//   props
+// ) => {
+//   //input value catching handler
+//   const updateMessageValue = (newMessageValue: string) => {
+//     props.dispatch(updateMessageTextAC(newMessageValue));
+//   };
+//   //message adding func
+//   const addMessage = () => {
+//     props.dispatch(addMessageAC());
+//   };
+//   return (
+//     <div>
+//       <Dialogues
+//         messagePage={props.messagePage}
+//         newMessageText={props.newMessageText}
+//         updateMessageValue={updateMessageValue}
+//         addMessage={addMessage}
+//       />
+//     </div>
+//   );
+// };
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,14 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import { store } from './redux/redux-store';
+import { Provider } from 'react-redux';
 
 const renderEntireTree = () => {
   ReactDOM.render(
     <BrowserRouter>
-      <App state={store.getState()} dispatch={store.dispatch} />
+      <Provider store={store}>
+        <App state={store.getState()} dispatch={store.dispatch} />
+      </Provider>
     </BrowserRouter>,
     document.getElementById('root')
   );
