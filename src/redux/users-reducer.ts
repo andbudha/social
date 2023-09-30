fd76fcd2d1b346fcaf719def037b15947494398d
andbudha <andreibartov@gmail.com>
2023-09-30 22:19:21 +0200

    users-reducer and initioal state created

---
+++ src/redux/users-reducer.ts
@@ -0,0 +1,45 @@
+const initialState = [
+  {
+    id: 1,
+    followed: true,
+    firstName: 'Andrei',
+    secondName: 'Bartov',
+    position: 'unemployed',
+    location: { country: 'Germany', city: 'Berlin' },
+  },
+  {
+    id: 2,
+    followed: false,
+    firstName: 'Delia',
+    secondName: 'Bartov',
+    position: 'pupil',
+    location: { country: 'Germany', city: 'Dresden' },
+  },
+  {
+    id: 3,
+    followed: true,
+    firstName: 'Natalia',
+    secondName: 'Bartov',
+    position: 'teacher',
+    location: { country: 'Germany', city: 'Berlin' },
+  },
+  {
+    id: 4,
+    followed: false,
+    firstName: 'Dorian',
+    secondName: 'Bartov',
+    position: 'student',
+    location: { country: 'Germany', city: 'Frankfurt' },
+  },
+];
+
+export const UsersReducer = (state: any, action: any) => {
+  switch (action.type) {
+    case 'XXX': {
+      return state;
+    }
+    default: {
+      return state;
+    }
+  }
+};
