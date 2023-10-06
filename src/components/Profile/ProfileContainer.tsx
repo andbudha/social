a6ffb418d2c6571fbf66fe9e61fcff4f3670b992
andbudha <andreibartov@gmail.com>
2023-10-06 10:03:52 +0200

    wrapper for withRouter hoc created/updated

--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ComponentType } from 'react';
 import { Profile } from './Profile';
 import { connect } from 'react-redux';
 import axios from 'axios';
@@ -6,11 +6,14 @@ import { AppRootStateType } from '../../redux/redux-store';
 import { Dispatch } from 'redux';
 import { UserProfileType } from '../../types/store-types';
 import { setUserProfileAC } from '../../redux/profile-reducer';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { withRouter } from '../../hocs/withRouter';
 
 export class ProfileAPIContainer extends React.Component<ProfileContainerPropsType> {
   componentDidMount() {
+    let profileID = this.props.params;
     axios
-      .get(`https://social-network.samuraijs.com/api/1.0/profile/${2}`)
+      .get(`https://social-network.samuraijs.com/api/1.0/profile/${profileID}`)
       .then((response) => {
         this.props.setUserProfile(response.data);
       });
@@ -44,8 +47,15 @@ const mapDispatchToProps = (dispatch: Dispatch): mapDispatchToPropsType => {
 };
 
 export type ProfileContainerPropsType = mapStateToPropsType &
-  mapDispatchToPropsType;
+  mapDispatchToPropsType &
+  WithRouterProps;
+export interface WithRouterProps {
+  location: ReturnType<typeof useLocation>;
+  params: Record<string, string>;
+  navigate: ReturnType<typeof useNavigate>;
+}
+
 export const ProfileContainer = connect(
   mapStateToProps,
   mapDispatchToProps
-)(ProfileAPIContainer);
+)(withRouter(ProfileAPIContainer));
---
+++ src/hocs/withRouter.tsx
@@ -0,0 +1,13 @@
+import { ComponentType } from 'react';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
+
+export function withRouter<T>(Component: ComponentType<T>) {
+  function ComponentWithRouterProp(props: any) {
+    let location = useLocation();
+    let navigate = useNavigate();
+    let params = useParams();
+    return <Component {...props} router={{ location, navigate, params }} />;
+  }
+
+  return ComponentWithRouterProp;
+}
