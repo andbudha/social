bba6f2c4324b44b6e99eec2a37209b048ad876a3
andbudha <andreibartov@gmail.com>
2023-10-12 22:10:28 +0200

    Login comp updated with redux form

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,11 +1,32 @@
+import { InjectedFormProps } from 'redux-form';
 import styles from './Login.module.css';
-import { LoginForm } from './LoginForm';
 
 export const Login = () => {
   return (
     <div>
       <h1>Login</h1>
       <LoginForm />
+    </div>
+  );
+};
+
+export const LoginForm: React.FC<InjectedFormProps> = (props) => {
+  return (
+    <div>
+      <form>
+        <div>
+          <input type="text" placeholder="Login" />
+        </div>
+        <div>
+          <input type="text" placeholder="Password" />
+        </div>
+        <div>
+          <input type="checkbox" /> Remeber me
+        </div>
+        <div>
+          <button>Login</button>
+        </div>
+      </form>
     </div>
   );
 };
