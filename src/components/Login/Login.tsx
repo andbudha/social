8a99faf5b5302ec80cdb9c1d2e01fbf0b21dcc9c
andbudha <andreibartov@gmail.com>
2023-10-12 22:08:08 +0200

    LoginForm component added

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,5 +1,11 @@
 import styles from './Login.module.css';
+import { LoginForm } from './LoginForm';
 
 export const Login = () => {
-  return <div>Login Page</div>;
+  return (
+    <div>
+      <h1>Login</h1>
+      <LoginForm />
+    </div>
+  );
 };
--- src/components/Profile/ProfileDetails/ProfileStatus.tsx
+++ src/components/Profile/ProfileDetails/ProfileStatus.tsx
@@ -10,7 +10,6 @@ export const ProfileStatus: React.FC<ProfileStatusPropsType> = (props) => {
   const [editMode, setEditMode] = useState(false);
   const [profileStatus, setProfileStatus] = useState('');
 
-  console.log('rendered');
   useEffect(() => {
     setProfileStatus(props.profileContainerProps.profileStatus);
   }, []);
@@ -54,57 +53,3 @@ export const ProfileStatus: React.FC<ProfileStatusPropsType> = (props) => {
     </div>
   );
 };
-
-// import React, { ChangeEvent } from 'react';
-// import styles from './ProfileStatus.module.css';
-// import { ProfileContainerPropsType } from '../ProfileContainer';
-
-// type ProfileStatusPropsType = {
-//   profileContainerProps: ProfileContainerPropsType;
-// };
-// export class ProfileStatus extends React.Component<ProfileStatusPropsType> {
-//   state = {
-//     editMode: false,
-//     status: '',
-//   };
-
-//   setOnEditModeHandler = () => {
-//     this.setState({
-//       editMode: true,
-//     });
-//   };
-
-//   setOffEditModeHandler = () => {
-//     this.setState({
-//       editMode: false,
-//     });
-//   };
-
-//   inputValuegettingHandler(event: ChangeEvent<HTMLInputElement>) {
-//     console.log(event.currentTarget.value);
-//     this.setState({ status: event.currentTarget.value });
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         {!this.state.editMode && (
-//           <div onDoubleClick={this.setOnEditModeHandler}>
-//             {this.props.profileContainerProps.profileStatus || 'No status'}
-//           </div>
-//         )}
-//         {this.state.editMode && (
-//           <div onBlur={this.setOffEditModeHandler.bind(this)}>
-//             <input
-//               autoFocus
-//               onChange={this.inputValuegettingHandler}
-//               // value={this.state.status || ''}
-//               className={styles.status_input}
-//               type="text"
-//             />
-//           </div>
-//         )}
-//       </div>
-//     );
-//   }
-// }
