51998a03780588ab925630e740430522f22ddd88
andbudha <andreibartov@gmail.com>
2023-11-01 21:39:16 +0100

    setAppErrorAC added

--- src/redux/app-reducer.ts
+++ src/redux/app-reducer.ts
@@ -4,17 +4,22 @@ const initialState: AppReducerInitialStateType = {
   error: null,
 };
 
-export type AppReducerActionTypes = any;
+export type AppReducerActionTypes = setAppErrorAcType;
 export const AppReducer = (
   state: AppReducerInitialStateType = initialState,
   action: AppReducerActionTypes
 ): AppReducerInitialStateType => {
   switch (action.type) {
-    case 'XXX': {
-      return state;
+    case 'app-reducer/SET-APP-ERROR': {
+      return { ...state, error: action.payload.error };
     }
     default: {
       return state;
     }
   }
+};
+
+type setAppErrorAcType = ReturnType<typeof setAppErrorAC>;
+export const setAppErrorAC = (error: string) => {
+  return { type: 'app-reducer/SET-APP-ERROR', payload: { error } } as const;
 };
